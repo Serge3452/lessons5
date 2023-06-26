7fce86562a902f8c4669061cde9921498a7a4e70
Serge3452 <serge3453@gmail.com>
2023-06-26 16:50:17 +0500

    Lessons 5
    Lessons 5

--- main.js
+++ main.js
@@ -1,16 +1,65 @@
+//Задание - Урок 5
 
+// Задание 1
+let a=1;
+ while(a<=50) {
+   console.log(a);
+    a++;
+ }  
 
+//for (let i=1; i<=50; i++) {
+//    console.log(i);
+//}
+
+// Задание 2
+for (let a=50; a>=25; --a){
+    console.log(a);
+}
+
+// Задание 3
+
+for (let a=2500; a<=3000; a++){
+    if ( a%2==0) {
+        console.log(a);
+    }
+}
 // Урок 5 из GitHub
 
-let i= 0;
-while (i<=10) {
-    if(i !=0){
-        console.log(i);}
-    i+=2;
-}
+//let i= 0;
+//while (i<=10) {
+//    if(i !=0){
+//        console.log(i);}
+//    i+=2;
+//}
 
 //let a=0
 //console.log(++a);
 
 //let a=2;
 //console.log(--a);
+
+// let i=0
+// do {
+//     console.log(i);
+//     i++
+// } while(i>10)
+
+// for (let i = 0; i < 10 ; i++) {
+//     console.log(i);
+// }
+
+// let s=0;
+// for(;;){
+//     console.log(s++);
+//     if (s>15) 
+//       break
+// }
+
+// let s=0;
+// for(;;){
+//     console.log(s++);
+//     if (s>15) 
+//       break
+// }
+// continue;
+
